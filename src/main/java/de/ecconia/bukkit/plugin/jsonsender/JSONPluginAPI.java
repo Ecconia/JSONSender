cb32a9a6d1a4cd0558d51a38b51f4aa33491ca86
Ecconia <github-commit@ecconia.com>
2018-07-26 16:47:28 +0200

    Javadoc

--- src/main/java/de/ecconia/bukkit/plugin/jsonsender/JSONPluginAPI.java
+++ src/main/java/de/ecconia/bukkit/plugin/jsonsender/JSONPluginAPI.java
@@ -4,7 +4,22 @@ import org.bukkit.entity.Player;
 
 public interface JSONPluginAPI
 {
+	/**
+	 * Attempts to send JSON-Text to a player by reflection or tellraw.
+	 * 
+	 * @param player Player which receives the JSON-Text
+	 * @param json JSON-Text which will be sent to the player
+	 * @return boolean - true if json has been sent to player
+	 */
 	boolean json(Player player, String json);
 	
+	/**
+	 * Attempts to send JSON-Text to a player by reflection or tellraw, else it sends the provided fallback.
+	 * 
+	 * @param player Player which receives the JSON-Text
+	 * @param json JSON-Text which will be sent to the player
+	 * @param fallback message which will be sent in case that reflection or tellraw fails
+	 * @return boolean - true if json has been sent to player by refelection or tellraw
+	 */
 	boolean json(Player player, String json, String fallback);
 }
